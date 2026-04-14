e6fe973e4939c48fda4042956fc335be0bb8bfc0
Nutmos <nuttapong_mos@hotmail.com>
2026-04-14 10:34:53 +0800

    add get all images

--- images-running-in-ocp/get-all-images.sh
+++ images-running-in-ocp/get-all-images.sh
@@ -2,3 +2,9 @@
 
 # List for the entire cluster
 oc get pods -A -ojsonpath='{range .items[*].status.containerStatuses[*]}{.image}{"\n"}{end}'
+
+oc get pods -A -ojsonpath='{range .items[*].status.containerStatuses[*]}{.imageID}{"\n"}{end}'
+
+oc get pods -A -ojsonpath='{range .items[*].status.initContainerStatuses[*]}{.image}{"\n"}{end}'
+
+oc get pods -A -ojsonpath='{range .items[*].status.initContainerStatuses[*]}{.imageID}{"\n"}{end}'
